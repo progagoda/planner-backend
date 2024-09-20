9fa0ef9b8cc69bf52540c8a04c4998c214d92e69
Brudar Artem Sergeevich <71312371+progagoda@users.noreply.github.com>
2024-09-20 12:41:26 +0300

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = await app.get(ConfigService);
   const port = config.get<number>('API_PORT');
+  app.enableCors();
   await app.listen(port || 3000, () => {
     console.log(`App started on port: ${port}`);
   });
